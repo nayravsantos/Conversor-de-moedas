fb909d41ecce79309debce5cc575d111518c20a4
Nayra Vitória dos Santos <santos.nayra@alunoescolassociais.org.br>
2024-10-24 12:10:23 -0300

    Inserindo códigos no JavaScript.

--- script.js
+++ script.js
@@ -25,5 +25,17 @@ document.getElementById('reset').addEventListener('click', function() {
 
 // Código Professor
 document.getElementById('currency-form').addEventListener('submit', function(event){
-    
+    event.preventDefault();
+
+    // Obter valores de entrada informados pelo usúario
+    const valor = parseFloat(document.getElementById('amount').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+
+    // Definir taxas de câmbio fixas
+    const exchangeRates = {
+        USD: { BRL: 5.70, EUR: 0.93 },
+        BRL: { USD: 0.18, EUR: 0.16 },
+        EUR: { USD: 1.08, BRL: 6.16 }
+    };
 });
